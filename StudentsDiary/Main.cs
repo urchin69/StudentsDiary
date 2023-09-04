5bcacb517b0f97a8fab7730952f00f436718401b
urchin69 <urchin@onet.plgit config  --global user.email urchin@onet.plgit config  --global user.email urchin@onet.pl>
2023-09-04 12:44:41 +0200

    MessageBox Właściwości

--- StudentsDiary/Main.cs
+++ StudentsDiary/Main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,29 @@ namespace StudentsDiary
 {
     public partial class Main : Form
     {
-        public Main()
+        public Main()//konstruktor
         {
             InitializeComponent();
+
+            var path = $@"{ Path.GetDirectoryName(Application.ExecutablePath)}\..\NowyPlik2.txt";
+
+            //if (!File.Exists(path))
+            //{
+            //    //System.IO.File.Create("C:\\Users\BONDEX1\\source\\repos\\StudentsDiary\\StudentsDiary"); // dwa slesze albo @ przed slashem
+            //    File.Create(path);
+            //}
+            //File.Delete(path);
+
+            //File.WriteAllText(path, "Zostań Programistą . NET");
+            File.AppendAllText(path, "Zostań Programistą . NET");
+
+
+            var text = File.ReadAllText(path);
+
+            MessageBox.Show(text);
+
+            MessageBox.Show("Test", "TYTUŁ", MessageBoxButtons.YesNoCancel,MessageBoxIcon.Error);
+
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
